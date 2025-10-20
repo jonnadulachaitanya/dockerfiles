d4895b3ea6f24b1274941a70e2c729d440bdb422
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-10-21 00:33:29 +0530

    file committed

---
+++ Dockerinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo dnf -y install dnf-plugins-core
+#or sudo yum install -y yum-utils
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl start docker
+usermod -aG docker ec2-user
